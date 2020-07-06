9fbc36e6e73873967440cc77b680c1982a7ed141
Marcos F. Calegari Andrade <mandrade@princeton.edu>
2020-07-06 08:59:06 -0400

    [Day-2] New files

---
+++ tutorial/Day-2/01-explore/iter.002/extract_to_retrain.sh
@@ -0,0 +1,25 @@
+cat << EOF > large_std.py
+import numpy as np
+
+f=np.genfromtxt('model_devi.out')
+f=f[::5]
+f=f.transpose()
+
+filter=np.logical_and(f[4]>0.025,f[4]<1.0)
+
+for i in f[0][filter]:
+  print(int(i))
+EOF
+
+python large_std.py > std_ind
+
+[ -e coord.raw ] && rm coord.raw
+
+for index in `cat std_ind`
+do
+  grep -x $index -B 1 -A 15 pos.lammpstrj > tmp
+  tail -n 8 tmp | sort -gk 1 | awk '{printf "%12.8f %12.8f %12.8f", $3*40,$4*40,$5*40}' >> coord.raw  
+  echo "" >> coord.raw
+done
+
+rm std_ind large_std.py tmp
